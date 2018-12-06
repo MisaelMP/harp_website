beff96ff89e184b56687ac213d737fd4100ff8b0
Misael M <misaelmdev@gmail.com>
2018-12-06 21:42:59 +1100

    added google maps to contact page

---
+++ scripts/map.js
@@ -0,0 +1,50 @@
+var map, infoWindow;
+    function initMap() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 6,
+        disableDefaultUI: true
+      });
+      infoWindow = new google.maps.InfoWindow;
+
+      // custom marker
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: {
+    		path: 'M0-48c-9.8 0-17.7 7.8-17.7 17.4 0 15.5 17.7 30.6 17.7 30.6s17.7-15.4 17.7-30.6c0-9.6-7.9-17.4-17.7-17.4z',
+    		fillColor: '#131615',
+    		fillOpacity: 1,
+    		strokeColor: '',
+    		strokeWeight: 0
+	}
+      });
+
+      // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('marker!');
+          infoWindow.open(map, marker);
+          marker.setPosition(pos);
+          map.setCenter(pos);
+        }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(browserHasGeolocation ?
+                            'Error: The Geolocation service failed.' :
+                            'Error: Your browser doesn\'t support geolocation.');
+      infoWindow.open(map, marker);
+    }
